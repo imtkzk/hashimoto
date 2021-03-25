2fd51a866563dd01d0d21a47d3788270773481ed
imtkzk <info@cullumdesign.com>
2021-03-25 10:20:20 +0900

    css

---
+++ js/imgModal.js
@@ -0,0 +1,74 @@
+$(function(){
+   $('.js-modal-open-1').on('click',function(){
+       $('.js-modal-1').fadeIn();
+       return false;
+   });
+   $('.js-modal-close').on('click',function(){
+       $('.js-modal-1').fadeOut();
+       return false;
+   });
+   $('.js-modal-open-2').on('click',function(){
+       $('.js-modal-2').fadeIn();
+       return false;
+   });
+   $('.js-modal-close').on('click',function(){
+       $('.js-modal-2').fadeOut();
+       return false;
+   });
+   $('.js-modal-open-3').on('click',function(){
+       $('.js-modal-3').fadeIn();
+       return false;
+   });
+   $('.js-modal-close').on('click',function(){
+       $('.js-modal-3').fadeOut();
+       return false;
+   });
+   $('.js-modal-open-4').on('click',function(){
+       $('.js-modal-4').fadeIn();
+       return false;
+   });
+   $('.js-modal-close').on('click',function(){
+       $('.js-modal-4').fadeOut();
+       return false;
+   });
+   $('.js-modal-open-5').on('click',function(){
+       $('.js-modal-5').fadeIn();
+       return false;
+   });
+   $('.js-modal-close').on('click',function(){
+       $('.js-modal-5').fadeOut();
+       return false;
+   });
+   $('.js-modal-open-6').on('click',function(){
+       $('.js-modal-6').fadeIn();
+       return false;
+   });
+   $('.js-modal-close').on('click',function(){
+       $('.js-modal-6').fadeOut();
+       return false;
+   });
+   $('.js-modal-open-7').on('click',function(){
+       $('.js-modal-7').fadeIn();
+       return false;
+   });
+   $('.js-modal-close').on('click',function(){
+       $('.js-modal-7').fadeOut();
+       return false;
+   });
+   $('.js-modal-open-8').on('click',function(){
+       $('.js-modal-8').fadeIn();
+       return false;
+   });
+   $('.js-modal-close').on('click',function(){
+       $('.js-modal-8').fadeOut();
+       return false;
+   });
+   $('.js-modal-open-9').on('click',function(){
+       $('.js-modal-9').fadeIn();
+       return false;
+   });
+   $('.js-modal-close').on('click',function(){
+       $('.js-modal-9').fadeOut();
+       return false;
+   });
+ });
